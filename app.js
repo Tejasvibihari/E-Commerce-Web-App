36cc87b4859779d6260cf597faa70dfa5945ab72
Tejasvi Bihari <tejasvibihari2000@gmail.com>
2023-10-25 18:07:11 +0530

    created some page for product categories

--- app.js
+++ app.js
@@ -3,8 +3,13 @@ import express from "express";
 import mongoose from "mongoose";
 import multer from "multer";
 
+
+
+
+
 const app = express();
 const port = 3000;
+
 
 app.use(express.static("public"));
 app.get("/", (req, res) => {
@@ -22,9 +27,40 @@ app.get("/kids", (req, res) => {
 app.get("/footwear", (req, res) => {
     res.render("footwear.ejs");
 });
+
+
+// Admin Route 
+
 app.get("/seller", (req, res) => {
     res.render("seller.ejs");
 });
+app.get("/dashboard", (req, res) => {
+    res.render("seller.ejs");
+});
+app.get("/product", (req, res) => {
+    res.render("product.ejs");
+});
+app.get("/order", (req, res) => {
+    res.render("order.ejs");
+});
+app.get("/customers", (req, res) => {
+    res.render("customers.ejs");
+});
+app.get("/adminprofile", (req, res) => {
+    res.render("adminprofile.ejs");
+});
+
+
+
+
+
+
+
+
+
+
+
+
 
 app.listen(port, () => {
     console.log(`Server Started Successfully on Port ${port}`);
